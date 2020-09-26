cc8888b2d7d9aa62383599a8a1da1c9ddd67b6bb
Alie Stone <helloaliestone@gmail.com>
2020-09-26 18:16:58 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+    return true
+  else
+    return false
   end
+end
 
   def question?
-
+    if self.end_with?("?")
+    return true
+  else
+    return false
   end
+end
 
   def exclamation?
-
+    if self.end_with?("!")
+    return true
+  else
+    return false
   end
+end
 
   def count_sentences
+    sentence_array = self.split(/[.?!]/)
+    if self.empty?
+      return "0"
+    else
+    puts sentence_array.count
+  end
 
   end
 end
